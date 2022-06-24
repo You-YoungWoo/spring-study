c02a3ba9e0cbdc8e4d584e62235adfaabf014a6f
lifeNewbi <ytaeuk@naver.com>
2022-06-25 02:24:45 +0900

    스프링 데이터 JPA 설정 공부

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;  // 시스템이 저장하는 임의의 ID값
+
     private String name; // 이름
 
     public Long getId() {
