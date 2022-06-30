843f045f96304dac7086979277c8e7964ef3e51a
lifeNewbi <ytaeuk@naver.com>
2022-07-01 01:32:36 +0900

    thymeleaf, Controller 공부

--- code_SPB_project/src/main/java/com/study/code_spb_project/repository/MemoRepository.java
+++ code_SPB_project/src/main/java/com/study/code_spb_project/repository/MemoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.awt.print.Pageable;
 import java.util.List;
 
+
 public interface MemoRepository extends JpaRepository<Memo, Long>{
 
     List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
